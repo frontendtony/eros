a324bf58c5bfd8ef7169338c8339e1429bd0059e
Anthony Oyathelemhi <tonero91@gmail.com>
2023-06-29 01:05:23 +0100

    fetch user details

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -20,7 +20,7 @@ public class UsersController : ControllerBase
     [HttpPost(Name = "CreateUser")]
     public async Task<IActionResult> Create(UserModel user)
     {
-        if (!ModelState.IsValid)
+        if (!ModelState.IsValid || user.Password == null)
         {
             return BadRequest(ModelState);
         }
@@ -37,5 +37,28 @@ public class UsersController : ControllerBase
 
         user.Password = null;
         return Created("", user);
+    }
+
+    [HttpGet("{emailOrId}", Name = "GetUser")]
+    [ProducesResponseType(typeof(UserModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(UserModel), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get(string emailOrId)
+    {
+        var user = await _userManager.FindByEmailAsync(emailOrId);
+        if (user == null)
+        {
+            user = await _userManager.FindByIdAsync(emailOrId);
+        }
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new UserModel()
+        {
+            Email = user.Email,
+            UserName = user.UserName
+        });
     }
 }
