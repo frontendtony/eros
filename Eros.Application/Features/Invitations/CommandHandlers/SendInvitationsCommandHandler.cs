ebacddcbcd1d632dc632aca744cc3bae61f735cd
Anthony Oyathelemhi <tonero91@gmail.com>
2024-04-24 07:25:24 +0100

    apply editor lint suggestions

--- Eros.Application/Features/Invitations/CommandHandlers/SendInvitationsCommandHandler.cs
+++ Eros.Application/Features/Invitations/CommandHandlers/SendInvitationsCommandHandler.cs
@@ -1,53 +1,55 @@
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+using Eros.Application.EmailService;
 using Eros.Application.Exceptions;
 using Eros.Application.Features.Invitations.Commands;
 using Eros.Domain.Aggregates.Estates;
 using Eros.Domain.Aggregates.Invitations;
 using Eros.Domain.Aggregates.Roles;
 using Eros.Domain.Aggregates.Users;
-using Eros.Persistence;
 using MediatR;
-using Eros.Application.EmailService;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Eros.Application.Features.Invitations.CommandHandlers;
 
 public class SendInvitationsCommandHandler(
-    ErosDbContext _dbContext,
-    IRoleReadRepository _roleReadRepository,
-    IEstateUserReadRepository _estateUserReadRepository,
-    IUserReadRepository _userReadRepository,
-    IInvitationReadRepository _invitationReadRepository,
+    DbContext dbContext,
+    IRoleReadRepository roleReadRepository,
+    IEstateUserReadRepository estateUserReadRepository,
+    IUserReadRepository userReadRepository,
+    IInvitationReadRepository invitationReadRepository,
     IEmailClient _emailClient,
-    IInvitationWriteRepository _invitationWriteRepository,
-    ILogger<SendInvitationsCommandHandler> _logger
+    IInvitationWriteRepository invitationWriteRepository,
+    ILogger<SendInvitationsCommandHandler> logger
 ) : IRequestHandler<SendInvitationsCommand, bool>
 {
     public async Task<bool> Handle(SendInvitationsCommand request, CancellationToken cancellationToken)
     {
-        var estateUser = await _estateUserReadRepository.GetByEstateIdAndUserIdAsync(request.EstateId, request.SenderId, cancellationToken);
+        var estateUser = await estateUserReadRepository.GetByEstateIdAndUserIdAsync(request.EstateId, request.SenderId, cancellationToken);
 
         if (estateUser == null)
         {
-            _logger.LogError("User is not a member of the estate. {EstateId} {SenderId}", request.EstateId, request.SenderId);
+            logger.LogError("User is not a member of the estate. {EstateId} {SenderId}", request.EstateId, request.SenderId);
             throw new BadRequestException("You cannot send invitations to an estate you are not a member of.");
         }
 
-        var role = await _roleReadRepository.GetByIdAsync(request.RoleId);
+        var role = await roleReadRepository.GetByIdAsync(request.RoleId);
 
         if (role == null)
         {
-            _logger.LogError("The role does not exist. {RoleId}", request.RoleId);
+            logger.LogError("The role does not exist. {RoleId}", request.RoleId);
             throw new BadRequestException("The role does not exist.");
         }
 
-        var rolePermisionIds = role.Permissions.Select(p => p.Id);
+        var rolePermissionIds = role.Permissions.Select(p => p.Id);
 
-        var hasPermissions = await _roleReadRepository.HasPermissionsAsync(estateUser.RoleId, rolePermisionIds, cancellationToken);
+        var hasPermissions = await roleReadRepository.HasPermissionsAsync(estateUser.RoleId, rolePermissionIds, cancellationToken);
 
         if (!hasPermissions)
         {
             // User can only assign roles with permissions that he has
-            _logger.LogError("User does not have the required permissions to assign the role. {EstateId} {SenderId} {RoleId}", request.EstateId, request.SenderId, request.RoleId);
+            logger.LogError("User does not have the required permissions to assign the role. {EstateId} {SenderId} {RoleId}", request.EstateId, request.SenderId, request.RoleId);
             throw new BadRequestException("You can only assign roles with permissions that you have.");
         }
 
@@ -56,35 +58,35 @@ public class SendInvitationsCommandHandler(
         foreach (var email in request.Emails)
         {
             // check if the user already exists
-            var user = await _userReadRepository.GetByEmailAsync(email);
+            var user = await userReadRepository.GetByEmailAsync(email);
             if (user != null)
             {
                 // check if the user is an eros admin
                 if (user.IsAdmin)
                 {
-                    _logger.LogWarning("User is an Eros admin. {UserId}", user.Id);
+                    logger.LogWarning("User is an Eros admin. {UserId}", user.Id);
                     continue;
                 }
 
                 // check if the user is already a member of the estate
-                var existingEstateUser = await _estateUserReadRepository.GetByEstateIdAndUserIdAsync(request.EstateId, user.Id, cancellationToken);
+                var existingEstateUser = await estateUserReadRepository.GetByEstateIdAndUserIdAsync(request.EstateId, user.Id, cancellationToken);
                 if (existingEstateUser != null)
                 {
-                    _logger.LogWarning("User is already a member of the estate. {EstateId} {UserId}", request.EstateId, user.Id);
+                    logger.LogWarning("User is already a member of the estate. {EstateId} {UserId}", request.EstateId, user.Id);
                     continue;
                 }
             }
 
             // check if the user has an existing invitation for the estate with the same email
-            var existingInvitation = await _invitationReadRepository.GetByEstateIdAndEmailAsync(request.EstateId, email, cancellationToken);
+            var existingInvitation = await invitationReadRepository.GetByEstateIdAndEmailAsync(request.EstateId, email, cancellationToken);
             if (existingInvitation != null)
             {
-                _logger.LogWarning("User has an existing invitation for the estate. {EstateId} {Email}", request.EstateId, email);
+                logger.LogWarning("User has an existing invitation for the estate. {EstateId} {Email}", request.EstateId, email);
 
                 // check if the role is different
                 if (existingInvitation.RoleId != request.RoleId)
                 {
-                    _logger.LogWarning("User has an existing invitation for the estate with a different role. {EstateId} {Email} {RoleId} {ExistingRoleId}", request.EstateId, email, request.RoleId, existingInvitation.RoleId);
+                    logger.LogWarning("User has an existing invitation for the estate with a different role. {EstateId} {Email} {RoleId} {ExistingRoleId}", request.EstateId, email, request.RoleId, existingInvitation.RoleId);
                     existingInvitation.Cancel();
                 }
                 // extend the expiration date if the invitation is pending
@@ -93,7 +95,7 @@ public class SendInvitationsCommandHandler(
                     existingInvitation.ResetExpiration();
 
                     invitationsToSend.Add(existingInvitation);
-                    _invitationWriteRepository.UpdateAsync(existingInvitation, cancellationToken);
+                    invitationWriteRepository.UpdateAsync(existingInvitation, cancellationToken);
 
                     continue;
                 }
@@ -114,25 +116,25 @@ public class SendInvitationsCommandHandler(
             }
 
             invitationsToSend.Add(invitation);
-            await _invitationWriteRepository.AddAsync(invitation, cancellationToken);
+            await invitationWriteRepository.AddAsync(invitation, cancellationToken);
         }
 
         if (invitationsToSend.Count == 0)
         {
-            _logger.LogWarning("No invitations to send.");
+            logger.LogWarning("No invitations to send.");
             return false;
         }
 
-        await _dbContext.SaveChangesAsync(cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
 
-        Task.Run(() => SendInvitationEmails(invitationsToSend));
+        Task.Run(() => SendInvitationEmails(invitationsToSend), cancellationToken);
 
         return true;
     }
 
     private Task SendInvitationEmails(List<Invitation> invitations)
     {
-        _logger.LogInformation("Sending invitation emails...");
+        logger.LogInformation("Sending invitation emails...");
 
         foreach (var invitation in invitations)
         {
@@ -140,7 +142,7 @@ public class SendInvitationsCommandHandler(
             //send email
             //probably move this to a service that gets the email content
             //_emailClient.Send();
-            _logger.LogInformation("Invitation: {Email} {Id}", invitation.Email, invitation.Id);
+            logger.LogInformation("Invitation: {Email} {Id}", invitation.Email, invitation.Id);
         }
         return Task.CompletedTask;
     }
